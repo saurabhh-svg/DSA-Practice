4060ef53b604052733fe8a2b74fa65208cf31998
Saurabh Singh <singhsaurabh2901@gmail.com>
2022-02-19 23:23:02 +0530

    edit

--- BST/DeleteBST.cpp
+++ BST/DeleteBST.cpp
@@ -2,38 +2,130 @@
 #include "BST.h"
 #include <queue>
 using namespace std;
+
+BinaryTreeNode<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "Enter Root Data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+        return NULL;
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter Left child of " << front->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+        cout << "Enter Right child of " << front->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+        if (rightChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+void PrintLevelWise(BinaryTreeNode<int> *root)
+{
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+        if (front->left != NULL)
+        {
+            cout << "L" << front->left->data;
+            pendingNodes.push(front->left);
+        }
+        if (front->right != NULL)
+        {
+            cout << "R" << front->right->data;
+            pendingNodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
 int height(BinaryTreeNode<int> *node)
 {
     if (node == NULL)
         return 0;
     return 1 + max(height(node->left), height(node->right));
 }
-
-BinaryTreeNode<int> *DeleteinBST(BinaryTreeNode<int> *root, int key)
+BinaryTreeNode<int> *InPre(BinaryTreeNode<int> *root)
 {
-    if (root == NULL)
-    {
-        return NULL;
+    while(root && root->right!=NULL){
+        root = root->right;
     }
-    if (root->data == key)
-    {
-    }
-    if (key < root->data)
-        root->left = DeleteinBST(root->left, key);
-    else if (key > root->data)
-        root->right = DeleteinBST(root->right, key);
-    else
-    {
-        if (height(root->left) > height(root->right))
-        {
-        }
-    }
-
     return root;
 }
-int main()
+BinaryTreeNode<int> *InSucc(BinaryTreeNode<int> *root)
 {
+    while (root && root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
 }
 
-// for input copy paste ->
-//  1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
+    BinaryTreeNode<int> *DeleteinBST(BinaryTreeNode<int> * root, int key)
+    {
+        BinaryTreeNode<int> *q;
+
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        if (root->left == NULL && root->right == NULL)
+        {
+            free(root);
+            return NULL;
+        }
+        if (key < root->data)
+            root->left = DeleteinBST(root->left, key);
+        else if (key > root->data)
+            root->right = DeleteinBST(root->right, key);
+        else
+        {
+            if (height(root->left) > height(root->right))
+            {
+                q = InPre(root->left);
+                root->data = q->data;
+                root->left = DeleteinBST(root->left, q->data);
+            }
+            else
+            {
+                q = InSucc(root->right);
+                root->data = q->data;
+                root->right = DeleteinBST(root->right, q->data);
+            }
+        }
+
+        return root;
+    }
+    int main()
+    {
+        BinaryTreeNode<int> *root = takeInputLevelWise();
+        PrintLevelWise(root);
+        DeleteinBST(root,1);
+        PrintLevelWise(root);
+    }
+
+    // for input copy paste ->
+    //  1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
